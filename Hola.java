54ad809a320a8ffc406ed7545a91bf6d8c866d3a
Alberto Luebbert M. <albertoluebbert@ideashappy.com>
2024-04-24 11:54:54 -0600

    Clase Samantha

--- Hola.java
+++ Hola.java
@@ -1,5 +1,6 @@
 public class Hola {
     public static void main(String[] args) {
         System.out.println("Hoola mundi!!");
+        System.out.println("Hola mundo!!");
     }   
 }
---
+++ Samantha.java
@@ -0,0 +1,5 @@
+public class Samantha{
+    public static void main(String args[]){
+        System.out.println("Hola Samantha!!");
+    }
+}
