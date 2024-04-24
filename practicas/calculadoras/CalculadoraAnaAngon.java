cdc64823d80d9a01292bc610f25fa4ece17a0959
Ana Karina Angón González <karinaangon10@gmail.com>
2024-04-24 12:10:43 -0600

    calculadora completa

---
+++ practicas/calculadoras/CalculadoraAnaAngon.java
@@ -0,0 +1,24 @@
+package practicas.calculadoras;
+
+public class CalculadoraAnaAngon {
+    public static void main(String args[]){
+        // Los Valores que haremos calculo.
+
+        float numero1 = 30;
+        float numero2 = 40;
+
+        // Suma
+        float suma = numero1 + numero2;
+        System.out.println("Suma: "+suma);
+
+        float resta = numero1 - numero2;
+        System.out.println("Resta: "+resta);
+
+        float multiplicacion = numero1 * numero2;
+        System.out.println("Multiplicacion: "+multiplicacion);
+
+        float division = numero1 / numero2;
+        System.out.println("Division: "+division);
+}
+    
+}
