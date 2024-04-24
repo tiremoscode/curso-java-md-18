f9f5228d16456094b7c8e1da38b97fa78c24be97
Alberto Luebbert M. <albertoluebbert@ideashappy.com>
2024-04-24 11:29:30 -0600

    :sparkles: Creación del Curso

---
+++ Calculadora.java
@@ -0,0 +1,12 @@
+public class Calculadora {
+    public static void main(String args[]){
+            // Los Valores que haremos calculo.
+
+            int numero1 = 30;
+            int numero2 = 40;
+
+            // Suma
+            int suma = numero1 + numero2;
+            System.out.println("Suma: "+suma);
+    }
+}
---
+++ Hola.java
@@ -0,0 +1,5 @@
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("Hoola mundi!!");
+    }   
+}
