ee6791412a42627dbab12b84086eefd561b13be6
Evelin Alexia Mondragon Beltran <evlin.student@gmail.com>
2024-04-24 12:14:44 -0600

    CalculadoraEvelin: calculadora con operaciones basicas: suma, resta, multiplicacion, division y modulo y el uso de variables de tipo double, y aprendfiendo a mandar mensajes

---
+++ practicas/calculadoras/CalculadoraEvelin.java
@@ -0,0 +1,27 @@
+package practicas.calculadoras;
+
+/**
+ *Practica de clase 24/04/24
+ * @author Evelin Mondragon
+ */
+
+ public class CalculadoraEvelin{
+    public static void main(String[] args) {
+        //variables, los valores que haremos calculo
+        double num1=30.3;
+        double num2=40.5;
+        double suma=num1+num2;
+        double resta=num1-num2;
+        double multiplicacion=num1*num2;
+        double division=num1*num2;
+        double modulo=num1%num2;
+
+        System.out.println("** Suma: "+suma);
+        System.out.println("**Resta: "+resta);
+        System.out.println("**Multiplicacion: "+multiplicacion);
+        System.out.println("**Divison: "+division);
+        System.out.println("**Modulo: "+modulo);
+
+    }
+}  
+
