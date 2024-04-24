86d66d0b11f20d6aa6aa65991c45bfe0710343e1
Alberto Luebbert M. <albertoluebbert@ideashappy.com>
2024-04-24 11:36:12 -0600

    :sparkles: Calculadora Alberto

---
+++ practicas/calculadoras/CalculadoraAlberto.java
@@ -0,0 +1,14 @@
+package practicas.calculadoras;
+
+public class CalculadoraAlberto {
+    public static void main(String args[]){
+            // Los Valores que haremos calculo.
+
+            int numero1 = 30;
+            int numero2 = 40;
+
+            // Suma
+            int suma = numero1 + numero2;
+            System.out.println("Suma: "+suma);
+    }
+}
