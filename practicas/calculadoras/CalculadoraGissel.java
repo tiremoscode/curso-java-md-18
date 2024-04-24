688a4cd1b690ba582fa87b3c69d183e9d673852b
Gissel Contreras <contrerasgissel31@gmail.com>
2024-04-24 12:58:25 -0600

    Complete calculadora

---
+++ practicas/calculadoras/CalculadoraGissel.java
@@ -0,0 +1,23 @@
+package practicas.calculadoras;
+
+public class CalculadoraGissel {
+
+    public static void main(String args[]){
+        // Los Valores que haremos calculo.
+
+        float numero1 = 30;
+        float numero2 = 40;
+
+        // Suma
+        float suma = numero1 + numero2;
+        float resta = numero1 - numero2;
+        float mult = numero1 * numero2;
+        float div = numero1/numero2;
+
+        System.out.println("Suma: "+suma);
+        System.out.println("resta: "+resta);
+        System.out.println("multiplicación: "+mult);
+        System.out.println("div: "+div);
+}
+    
+}
