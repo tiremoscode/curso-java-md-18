d2e8db12f88324b4110524a25941a0c854d1089f
elizabeth calderon <eliz_15mychemi@hotmail.com>
2024-04-24 22:32:06 -0600

    creación de funciones aritmeticas dentro de una rama nueva

---
+++ practicas/calculadoras/CalculadoraElizabeth.java
@@ -0,0 +1,43 @@
+package practicas.calculadoras;
+
+public class CalculadoraElizabeth {
+    public static void main(String args[]){
+        // Los Valores que haremos calculo.
+
+        int numero1 = 30;
+        int numero2 = 40;
+        int numero3 = 50;
+
+        // Suma
+        int suma = numero1 + numero2 + numero3;
+        System.out.println("Suma: "+suma);
+}
+}
+
+
+public class CalculadoraElizabeth {
+    public static void main(String args[]){
+        // Los Valores que haremos calculo.
+
+        double numero1 = 100.0;
+        double numero2 = 87.6;
+
+        // Resta
+        int resta = numero1 - numero2;
+        System.out.println("Resta: "-resta);
+}
+}
+
+
+public class CalculadoraElizabeth {
+    public static void main(String args[]){
+        // Los Valores que haremos calculo.
+
+        float numero1 = 300.0;
+        float numero2 = 15.5;
+
+        // Resta
+        int division = numero1 / numero2;
+        System.out.println("division: "/division);
+}
+}
