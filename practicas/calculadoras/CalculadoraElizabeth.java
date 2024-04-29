d596705394000412aec232439e2293b92e558d17
elizabeth calderon <eliz_15mychemi@hotmail.com>
2024-04-28 23:07:32 -0600

    agrego calculadora a una nueva rama llamada Elizabeth

--- practicas/calculadoras/CalculadoraElizabeth.java
+++ practicas/calculadoras/CalculadoraElizabeth.java
@@ -2,42 +2,25 @@ package practicas.calculadoras;
 
 public class CalculadoraElizabeth {
     public static void main(String args[]){
-        // Los Valores que haremos calculo.
-
-        int numero1 = 30;
+  
+//los valores que haremos calculo
+        int numero1 = 25;
         int numero2 = 40;
-        int numero3 = 50;
-
-        // Suma
-        int suma = numero1 + numero2 + numero3;
-        System.out.println("Suma: "+suma);
-}
-}
-
-
-public class CalculadoraElizabeth {
-    public static void main(String args[]){
-        // Los Valores que haremos calculo.
-
-        double numero1 = 100.0;
-        double numero2 = 87.6;
-
-        // Resta
-        int resta = numero1 - numero2;
-        System.out.println("Resta: "-resta);
-}
-}
-
-
-public class CalculadoraElizabeth {
-    public static void main(String args[]){
-        // Los Valores que haremos calculo.
-
-        float numero1 = 300.0;
-        float numero2 = 15.5;
-
-        // Resta
-        int division = numero1 / numero2;
-        System.out.println("division: "/division);
+        int numero3 = 75;
+        int numero4 = 23;
+        int numero5 = 20;
+        int numero6 = 89;
+        int numero7 = 58;
+        int numero8 = 22; 
+        
+        int suma = numero1 + numero2;
+        int resta = numero3 - numero4;
+        int multiplicacion = numero5 * numero6;
+        int division = numero7 / numero8;
+    
+        System.out.println("la suma de estos numeros: "+suma);
+        System.out.println("la resta de estos numeros: "+resta);
+        System.out.println("la multiplicacion de estos numeros: "+multiplicacion);
+        System.out.println("la division de estos numeros: "+division);
 }
 }
