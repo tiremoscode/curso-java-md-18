aa1a57c43417bbc27efe4a85adf5e510a3296114
linz04xmc <163241199+linz04xmc@users.noreply.github.com>
2024-04-30 09:36:44 -0600

    Add files via upload

--- Calculadora.java
+++ Calculadora.java
@@ -1,12 +1,22 @@
 public class Calculadora {
-    public static void main(String args[]){
-            // Los Valores que haremos calculo.
+    public static void main(String[] args) {
+        int numero1 = 30;
+        int numero2 = 40;
 
-            int numero1 = 30;
-            int numero2 = 40;
+        // Suma
+        int suma = numero1 + numero2;
+        System.out.println("Suma: "+suma);
 
-            // Suma
-            int suma = numero1 + numero2;
-            System.out.println("Suma: "+suma);
+        // Resta
+        int resta = numero1 - numero2;
+        System.out.println("Resta: "+resta);
+
+        // Division 
+        int division = numero1 / numero2;
+        System.out.println("Division: "+ division);
+
+        // Multiplicación
+        int multiplicacion = numero1 * numero2;
+        System.out.println("multiplicacion: "+ multiplicacion);
     }
 }
