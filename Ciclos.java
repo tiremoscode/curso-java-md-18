aaeb8f1a0c0971e195f214d3e3d7921ac977f0ca
Alberto Luebbert M. <albertoluebbert@ideashappy.com>
2024-05-07 11:06:43 -0600

    :sparkles: Ejemplos Ciclos y Clases

--- Ciclos.java
+++ Ciclos.java
@@ -1,11 +1,11 @@
-public class Ciclos{
+public class Ciclos {
     public static void main(String[] args) {
         // for
         // es una estructura de control que repite
         // un bloque de código un número determinado de veces
 
-        for (int j = 0; j <= 200; j++){
-            System.out.println(j+ ".- No debo hablar en clase");
+        for (int j = 0; j <= 200; j++) {
+            System.out.println(j + ".- No debo hablar en clase");
         }
 
         // While
@@ -14,20 +14,20 @@ public class Ciclos{
 
         // Variable de inicio
         int x = 1;
-        while (x <= 5){
+        while (x <= 5) {
             System.out.println(x);
             x++;
         }
 
         // Do While
-        // Nos va a permitir ejecutarun bloque de código al menos una vez
+        // Nos va a permitir ejecutar un bloque de código al menos una vez
         // y después mientras una condición del código se mantenga verdadera.
 
         int numero = 10;
         do {
             System.out.println(numero);
             numero++;
-        } while (numero <=15);
+        } while (numero <= 15);
 
         // Arreglo de Alumnas del Grupo numero 18
         Alumnas[] alumnas = new Alumnas[3];
@@ -37,10 +37,9 @@ public class Ciclos{
         alumnas[1] = new Alumnas("Andrea", "Hernandez");
         alumnas[2] = new Alumnas("Dulce", "hernandez");
 
-        for (int al = 0; al < alumnas.length; al++){
+        for (int al = 0; al < alumnas.length; al++) {
             System.out.println(alumnas[al].getNombre() + " " + alumnas[al].getApellido());
         }
-
 
     }
 }
