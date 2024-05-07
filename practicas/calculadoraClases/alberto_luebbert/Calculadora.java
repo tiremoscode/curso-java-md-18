f60b58b83a123ab76de56a705b46cee0ef586751
Alberto Luebbert M. <albertoluebbert@ideashappy.com>
2024-05-07 11:12:21 -0600

    :sparkles: Tarea Practica Calculadora por Clases

---
+++ practicas/calculadoraClases/alberto_luebbert/Calculadora.java
@@ -0,0 +1,20 @@
+package practicas.calculadoraClases.alberto_luebbert;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Operaciones operacion = new Operaciones();
+
+        double valor1 = 30;
+        double valor2 = 31;
+
+        // Forma1
+        double suma = operacion.sumar(valor1, valor2);
+        System.out.println("Suma Ejemplo 1: " + suma);
+
+        // Forma 2
+        System.out.println("Suma Ejemplo 2: " + operacion.sumar(valor1, valor2));
+
+        // Resta
+        System.out.println("Resta " + operacion.restar(valor1, valor2));
+    }
+}
---
+++ practicas/calculadoraClases/alberto_luebbert/Operaciones.java
@@ -0,0 +1,14 @@
+package practicas.calculadoraClases.alberto_luebbert;
+
+public class Operaciones {
+    double valor1;
+    double valor2;
+
+    public double sumar(double valor1, double valor2) {
+        return valor1 + valor2;
+    }
+
+    public double restar(double valor1, double valor2) {
+        return valor1 - valor2;
+    }
+}
