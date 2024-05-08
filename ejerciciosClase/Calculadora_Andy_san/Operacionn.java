88be7031b672ad3f0da5b3231de93a8cc06bc409
unknown <andii1234@hotmal.com>
2024-05-08 13:36:51 -0600

    Agrego m Calculadora con metodos Andrea

---
+++ ejerciciosClase/Calculadora_Andy_san/Operacionn.java
@@ -0,0 +1,23 @@
+package ejerciciosClase.Calculadora_Andy_san;
+
+public class Operacionn {
+    
+        double valor1;
+        double valor2;
+    
+        public double sumar(double valor1, double valor2) {
+            return valor1 + valor2;
+        }
+        public double restar(double valor1, double valor2) {
+            return valor1 - valor2;
+        }
+        public double multiplicar(double valor1, double valor2) {
+            return valor1 + valor2;
+        }
+        public double dividir(double valor1, double valor2) {
+            return valor1 + valor2;
+        }
+        public double residuo(double valor1, double valor2) {
+            return valor1 + valor2;
+        }
+}
