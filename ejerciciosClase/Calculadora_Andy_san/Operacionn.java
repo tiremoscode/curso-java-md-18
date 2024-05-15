dcc6e51a6f020a22f028b85f02cbfca5505a6a6d
unknown <andii1234@hotmal.com>
2024-05-15 10:13:40 -0600

    Realizo cambios de signos

--- ejerciciosClase/Calculadora_Andy_san/Operacionn.java
+++ ejerciciosClase/Calculadora_Andy_san/Operacionn.java
@@ -12,12 +12,12 @@ public class Operacionn {
             return valor1 - valor2;
         }
         public double multiplicar(double valor1, double valor2) {
-            return valor1 + valor2;
+            return valor1 * valor2;
         }
         public double dividir(double valor1, double valor2) {
-            return valor1 + valor2;
+            return valor1 / valor2;
         }
         public double residuo(double valor1, double valor2) {
-            return valor1 + valor2;
+            return valor1 % valor2;
         }
 }
