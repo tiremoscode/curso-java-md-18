6268bddfaf912b722365256e5ae145797eda473c
Maricela Cruz <marycf60@gmail.com>
2024-05-19 15:15:55 -0600

    Ya la ultima vez

---
+++ EjerciciosClase/CalculadoraClases/maricela_cruz/Calculadora.java
@@ -0,0 +1,31 @@
+package EjerciciosClase.CalculadoraClases.maricela_cruz;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Operaciones operacion = new Operaciones();
+
+        double valor1 = 30;
+        double valor2 = 31;
+
+        // Forma1
+        double suma = operacion.sumar(valor1, valor2);
+        System.out.println("Suma Ejemplo 1: " + suma);
+
+        // Forma 2
+        System.out.println("Suma Ejemplo 2: " + operacion.sumar(valor1, valor2));
+
+        // Resta
+        System.out.println("Resta " + operacion.restar(valor1, valor2));
+
+        // Multiplicar
+        System.out.println("Multiplicar"+operacion.multiplicar(valor1,valor2));
+
+        //Division
+        System.out.println("Division"+operacion.dividir(valor1,valor2));
+
+        //Residuo
+        System.out.println("Residuo"+operacion.residuo(valor1, valor2));
+        
+    }
+}
