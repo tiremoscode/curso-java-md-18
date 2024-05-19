54bf6211e8ce9d85758e486da3ad4dd8a2a5e92d
Dulce Loreto Hernandez Mendez <dulceloretohernandez@gmail.com>
2024-05-18 23:50:24 -0600

    [200~:sparkles:~Agrego calculadora de métodos

---
+++ EjerciciosClase/CalculadoraClases/dulce_loreto/Operaciones.java
@@ -0,0 +1,24 @@
+package EjerciciosClase.CalculadoraClases.dulce_loreto;
+
+public class Operaciones {
+    double valor1;
+    double valor2;
+
+     public double sumar(double valor1, double valor2) {
+        return valor1 + valor2;
+     }
+
+      public double restar(double valor1, double valor2) {
+        return valor1 - valor2;
+      }
+
+      public double multiplicar(double valor1, double valor2) {
+        return valor1 * valor2;
+        
+      }
+
+      public double dividir(double valor1, double valor2) {
+        return valor1 / valor2;
+        
+      }
+}
