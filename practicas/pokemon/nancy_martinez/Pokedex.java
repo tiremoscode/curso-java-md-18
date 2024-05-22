d02a7f51dc2903543a79fef4bcfe960bca3e5922
Nancy Viridina Martínez Salas <naanvms@gmail.com>
2024-05-21 21:06:33 -0600

    practicapokedex

--- practicas/pokemon/nancy_martinez/Pokedex.java
+++ practicas/pokemon/nancy_martinez/Pokedex.java
@@ -4,9 +4,9 @@ public class Pokedex {
     public static void main(String[] args) {
         PokemonFuego charmander = new PokemonFuego("Charmander", 8.5, 50, 5);
         PokemonAgua squirtle = new PokemonAgua("Squirtle", 9.0, 50, 6);
-        PokemonElectrico pikachu = new PokemonElectrico("Pikachu", 6.0, 50, 8);
-        PokemonPsiquico azelf = new PokemonPsiquico("Azelf", 5.0, 50, 5);
-        PokemonPsiquico jirachi = new PokemonPsiquico("Jirachi", 8.0, 50, 4);
+        PokemonElectrico pikachu = new PokemonElectrico("Pikachu", 6.0, 80, 8);
+        PokemonPsiquico azelf = new PokemonPsiquico("Azelf", 5.0, 70, 5);
+        PokemonPsiquico jirachi = new PokemonPsiquico("Jirachi", 8.0, 70, 4);
 
  
 
@@ -22,6 +22,13 @@ public class Pokedex {
         // y el peso de Squirtle
         System.out.println("Edad de mi Charmander: " + charmander.getEdad());
         System.out.println("Peso  de mi Squirtle " + squirtle.getPeso());
+        System.out.println("Edad de mi pikachu:" + pikachu.getEdad());
+        System.out.println("Nivel de ataque:" + pikachu.getNivel());
+        System.out.println("Edad de mi azelf:" + azelf.getEdad());
+        System.out.println("Nivel de ataque:" + azelf.getNivel());
+        System.out.println("Edad de mi jirachi:" + jirachi.getEdad());
+        System.out.println("Nivel de ataque:" + jirachi.getNivel());
+
 
     }
 }
