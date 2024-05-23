df577084c4ca2a4b63165750f91ef34ae5d2d702
Clara Itzel Arreola Ramirez <citzelarreolar@gmail.com>
2024-05-22 20:33:53 -0600

    añado cambio de ortografia

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hoola mundi!!");
+        System.out.println("Hoola mundo!!");
     }   
 }
+
+//se corrige hola mundi//
