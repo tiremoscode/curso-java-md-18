85b30484bedfe841cc00d6dad2f154b4f66f493d
Paulina Gómez López <pauuliinagomez@gmail.com>
2024-05-26 16:34:59 -0600

    agrego calculadora

---
+++ practicas/calculadoras/Calculadora_Paulina.java
@@ -0,0 +1,25 @@
+package practicas.calculadoras;
+
+public class Calculadora_Paulina {
+    double valor1;
+    double valor2;
+
+     public double sumar(double valor1, double valor2) {
+        return valor1 + valor2;
+     }
+
+      public double restar(double valor1, double valor2) {
+        return valor1 - valor2;
+      }
+
+      public double multiplicar(double valor1, double valor2) {
+        return valor1 * valor2;
+        
+      }
+
+      public double dividir(double valor1, double valor2) {
+        return valor1 / valor2;
+        
+      }
+    
+}
