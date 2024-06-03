4963740683758313919e50547bee8a33fb4d4b5a
Linz Xally Melchor Cruz <linz4kawaii@gmail.com>
2024-06-03 06:29:33 -0600

    Ejercicion de Calculadora 2

---
+++ practicas/calculadoraClases/linz_melchor/Operaciones.java
@@ -0,0 +1,28 @@
+package linz_melchor;
+
+public class Operaciones {
+    double valor1;
+    double valor2;
+
+    public double sumar(double valor1, double valor2){
+        return valor1 + valor2;
+    }
+
+    public double restar(double valor1,  double valor2){
+        return valor1 - valor2;
+    }
+
+    public double multiplicar (double valor1, double valor2){
+        return valor1*valor2;
+    }
+
+    public double dividir (double valor1, double valor2){
+        return valor1/valor2;
+    }
+
+    public double residuo (double valor1, double valor2){
+        return valor1%valor2;
+    }
+
+
+}
