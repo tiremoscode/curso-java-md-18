12037cc91683907ca55a9f3ea8be22d130ab2b10
Fabiola <fabygarciasm@gmail.com>
2024-06-06 23:19:17 -0600

    agrego calculadora clases

---
+++ practicas/calculadoraClases/fabiola_hernandez/Operaciones.java
@@ -0,0 +1,22 @@
+package practicas.calculadoraClases.fabiola_hernandez;
+
+public class Operaciones {
+    double valor1;
+    double valor2;
+
+    public double sumar(double valor1, double valor2) {
+        return valor1 + valor2;
+    }
+
+    public double restar(double valor1, double valor2) {
+        return valor1 - valor2;
+    }
+
+        public double multiplicacion(double valor1, double valor2) { 
+            return valor1 * valor2;
+
+        }
+    public double division(double valor1, double valor2){
+        return valor1 / valor2;
+    }
+}
