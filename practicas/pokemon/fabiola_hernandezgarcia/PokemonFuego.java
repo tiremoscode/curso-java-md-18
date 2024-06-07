8ae9c695f03225e24679160b470c3866b67096ee
Fabiola <fabygarciasm@gmail.com>
2024-06-06 22:02:21 -0600

    :fire: agrego 3 pokemones

---
+++ practicas/pokemon/fabiola_hernandezgarcia/PokemonFuego.java
@@ -0,0 +1,18 @@
+package practicas.pokemon.fabiola_hernandezgarcia;
+
+class PokemonFuego extends Pokemon implements TipoPokemon {
+    public PokemonFuego(String nombre, double peso, int nivel, int edad) {
+        super(nombre, peso, nivel, edad);
+    }
+
+    @Override
+    public String getTipoPokemon() {
+        return "Fuego";
+    }
+
+    @Override
+    public void atacar(){
+        super.atacar();
+        System.out.println(getNombre()+ " lanza una llamarada!!");
+    }
+}
